86f3651325a9a8b8a3dd1facb0c46c6b796ea4e5
iniwahyu <111201609357@mhs.dinus.ac.id>
2018-08-03 20:44:23 +0700

    Fix Folder Home

---
+++ application/views/Home/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Print Media</title>
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/materialize.min.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/style.css">
+</head>
+
+<body>
+
+<div id="navbar">
+<nav>
+    <div class="nav-wrapper navbar">
+        <a href="#" class="brand-logo">Print Media</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="">Sass</a></li>
+            <li><a href="">Masuk</a></li>
+            <li><a href="">Daftar</a></li>
+        </ul>
+    </div>
+</nav>
+</div>
+
+<section>
+</section>
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="<?php echo base_url(); ?>asset/home/js/materialize.min.js"></script>
+</body>
+</html>
