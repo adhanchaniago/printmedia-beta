1e575b643e139c673b6a391e6cb1820816a48331
iniwahyu <111201609357@mhs.dinus.ac.id>
2018-08-05 13:26:18 +0700

    Initial Index

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -1,35 +1,264 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-<title>Print Media</title>
-<!--Let browser know website is optimized for mobile-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!--Import Google Icon Font-->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!--Import materialize.css-->
-<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/materialize.min.css"  media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/style.css">
-</head>
+<title>Print Media | Solusi Percetakan Masa Kini</title>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Material Design for Bootstrap fonts and icons -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+<!-- Material Design for Bootstrap CSS -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/bootstrap.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo base_url(); ?>asset/home/css/style.css">
 
+</head>
 <body>
 
 <div id="navbar">
-<nav>
-    <div class="nav-wrapper navbar">
-        <a href="#" class="brand-logo">Print Media</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="">Sass</a></li>
-            <li><a href="">Masuk</a></li>
-            <li><a href="">Daftar</a></li>
-        </ul>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo base_url('/'); ?>">PrintMedia</a>
+            <button class="navbar-toggler btn btn-outline-primary" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fitur</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link kotak kotak-biru" href="#">Masuk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link kotak kotak-hijau" href="#">Daftar</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </div>
 
-<section>
+
+<section class="section section-intro" id="mulai">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="intro-kiri">
+                    <h2>PrintMedia</h2>
+                    <p>Donec a tortor tortor. Phasellus in vulputate felis, nec viverra tellus. Sed tempus enim imperdiet, semper ex et, laoreet massa. Ut in fringilla sapien. Phasellus eu laoreet metus. Suspendisse non nunc id neque varius sodales. Nulla et metus in ipsum scelerisque condimentum. In varius venenatis elit ac semper. Sed ultricies volutpat lacus sit amet porttitor. Mauris fringilla lacus sed diam aliquet, ac lacinia elit fermentum. Proin urna purus, interdum et leo quis, lacinia aliquam nunc.</p>
+                    <ul>
+                        <li><a href="" class="kotak kotak-kiri">ABOUT US</a></li>
+                        <li><a href="" class="kotak kotak-kanan">ABOUT US</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="<?php echo base_url('asset/home/img/intro-foto.png'); ?>" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div> 
 </section>
 
-<!--JavaScript at end of body for optimized loading-->
-<script type="text/javascript" src="<?php echo base_url(); ?>asset/home/js/materialize.min.js"></script>
+<section class="section section-tutorial">
+    <div class="container">
+        <div class="default-header">
+            <h3>LANGKAH PEMESANAN</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="tutorial-header">
+                    <img src="<?php echo base_url('asset/home/img/register.png'); ?>" class="img-fluid" width="150" height="150">
+                </div>
+                <div class="tutorial-body">
+                    <h4>Daftar di PrintMedia</h4>
+                    <p>User melakukan pendaftaran di Print Media dan melengkapi data-datanya.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="tutorial-header">
+                    <img src="<?php echo base_url('asset/home/img/upload-dokumen.png'); ?>" class="img-fluid" width="150" height="150">
+                </div>
+                <div class="tutorial-body">
+                    <h4>Upload Dokumen</h4>
+                    <p><b>1.</b> Anda mengunggah dokumen dengan format-format yang di inginkan.</p>
+                    <p><b>2.</b> Pilih jenis pembayaran dan pengiriman.</p>
+                    <p><b>3.</b> Pihak Print Media akan memproses pesanan Anda.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="tutorial-header">
+                    <img src="<?php echo base_url('asset/home/img/shipping.png'); ?>" class="img-fluid" width="150" height="150">
+                </div>
+                <div class="tutorial-body">
+                    <h4>Dokumen Sampai</h4>
+                    <p>Dokumen akan sampai sesuai dengan waktu yang telah ditentukan. Jika dalam waktu 7 hari tidak sampai, maka akan diberikan pengembalian uang.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-layanan">
+    <div class="container">
+        <div class="default-header">
+            <h3>LAYANAN KAMI</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo base_url('asset/home/img/file.svg'); ?>" class="card-img-top" width="64" height="64">
+                    </div>
+                    <div class="card-body">
+                        <h4>Kualitas Terbaik</h4>
+                        <p class="card-text">Seluruh data pembelian anda setiap harinya akan kami simpan dengan baik, bukan hanya harian saja bahkan data pembelian bulanan anda juga akan kami simpan.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo base_url('asset/home/img/money.svg'); ?>" class="card-img-top" width="64" height="64">
+                    </div>
+                    <div class="card-body">
+                        <h4>Harga Murah</h4>
+                        <p class="card-text">Seluruh data pembelian anda setiap harinya akan kami simpan dengan baik, bukan hanya harian saja bahkan data pembelian bulanan anda juga akan kami simpan.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-img">
+                        <img src="<?php echo base_url('asset/home/img/shipping.svg'); ?>" class="card-img-top" width="64" height="64">
+                    </div>
+                    <div class="card-body">
+                        <h4>Hemat Waktu</h4>
+                        <p class="card-text">Seluruh data pembelian anda setiap harinya akan kami simpan dengan baik, bukan hanya harian saja bahkan data pembelian bulanan anda juga akan kami simpan.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section section-harga">
+    <div class="container">
+        <div class="default-header">
+            <h3>HARGA</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>KERTAS A4</h4>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Kualitas Kertas Terbaik</li>
+                        <li class="list-group-item">Bisa Hitam Putih dan Berwarna</li>
+                        <li class="list-group-item">Harga Mulai Rp. 150/lembar</li>
+                    </ul>
+                    <div class="card-footer">
+                        <a href="" class="btn btn-primary">PESAN SEKARANG</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>KERTAS F4</h4>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Kualitas Kertas Terbaik</li>
+                        <li class="list-group-item">Bisa Hitam Putih dan Berwarna</li>
+                        <li class="list-group-item">Harga Mulai Rp. 150/lembar</li>
+                    </ul>
+                    <div class="card-footer">
+                        <a href="" class="btn btn-primary">PESAN SEKARANG</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="footer">
+    <div class="container">
+        <div class="kelebihan">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php echo base_url('asset/home/img/aman.png'); ?>" alt="" width="75" height="75">
+                    <h4>Aman</h4>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="<?php echo base_url('asset/home/img/customer.png'); ?>" alt="" width="75" height="75">
+                    <h4>24/7 Support</h4>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="<?php echo base_url('asset/home/img/pembayaran.png'); ?>" alt="" width="75" height="75">
+                    <h4>Berbagai Pembayaran</h4>
+                </div>
+            </div>
+        </div>
+        <div class="about">
+            <div class="row">
+                <div class="col-md-3">
+                    logo
+                </div>
+                <div class="col-md-3">
+                    Perusahaan
+                </div>
+                <div class="col-md-3">
+                    Bantuan
+                </div>
+                <div class="col-md-3">
+                    Social Media
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container-fluid">
+        <p>© 2018 - PT Print Media</p>
+        </div>
+    </div>
+</section>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+<script>
+$(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('#mulai');
+    var offset = startchange.offset();
+    if (startchange.length){
+    $(document).scroll(function() { 
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            $(".navbar").css('background-color', '#fff');
+        } 
+        else {
+            $('.navbar').css('background-color', 'transparent');
+       }
+    });
+    }
+});
+</script>
+
 </body>
 </html>
