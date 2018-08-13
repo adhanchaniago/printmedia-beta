7f8dd1208e8b74479d9d56bd82ee57306fddcc4c
bugisetiawan <bugisetiawan98@gmail.com>
2018-08-14 01:41:50 +0700

    New Database

--- printmedia.sql
+++ printmedia.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2018 at 02:12 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Generation Time: Aug 13, 2018 at 08:41 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,22 +31,23 @@ SET time_zone = "+00:00";
 CREATE TABLE `activity_user` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `ip_address` varchar(30) NOT NULL,
-  `browser` int(30) NOT NULL,
-  `device` int(30) NOT NULL,
-  `jam_login` time(6) NOT NULL
+  `alamat_ip` varchar(100) NOT NULL,
+  `browser` varchar(255) NOT NULL,
+  `waktu_masuk` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `auth`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE `auth` (
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `level` varchar(10) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -56,10 +57,18 @@ CREATE TABLE `login` (
 --
 
 CREATE TABLE `user` (
-  `email` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `no_hp` varchar(20) NOT NULL,
-  `alamat` varchar(100) NOT NULL
+  `nohape` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `detail_alamat` varchar(100) NOT NULL,
+  `provinsi` varchar(100) NOT NULL,
+  `kota` varchar(100) NOT NULL,
+  `kecamatan` varchar(100) NOT NULL,
+  `kodepos` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -70,19 +79,23 @@ CREATE TABLE `user` (
 -- Indexes for table `activity_user`
 --
 ALTER TABLE `activity_user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
 
 --
--- Indexes for table `login`
+-- Indexes for table `auth`
 --
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`email`);
+ALTER TABLE `auth`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `token` (`token`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -92,6 +105,18 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `activity_user`
 --
 ALTER TABLE `activity_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth`
+--
+ALTER TABLE `auth`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
