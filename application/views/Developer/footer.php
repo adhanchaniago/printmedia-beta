572ea7260ae84486248dd245c177388cb4a7295f
abdielreyhan <abdielreyhan98@gmail.com>
2018-11-11 20:22:15 +0700

    footer

--- application/views/Developer/footer.php
+++ application/views/Developer/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2018 <a href="#">PrintMedia</a>.</strong>
-    All rights reserved.
+    Printmedia &copy; <?php echo date('Y'); ?> Eksekusi Time: {elapsed_time} s | Memory Usage: {memory_usage}
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.0-alpha
     </div>
